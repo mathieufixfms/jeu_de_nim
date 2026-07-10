d37b71a133f799cf82a2eaa94eb420041b86ca5f
Mathieu FIX <mathieu.fix@fms-ea.com>
2026-07-10 14:20:44 +0200

    jeu de nim version 2

---
+++ jeu_de_nim_v_2.py
@@ -0,0 +1,11 @@
+# /usr/bin/env python
+# -*-coding: utf-8-*-
+"""
+Jeu de Nim
+Deux joueurs s’affrontent dans ce jeu en se partageant un tas d’allumettes composé au départ de 21
+allumettes.
+Chaque joueur à son tour enlève entre une et quatre allumettes du tas. Celui qui enlève la dernière
+allumette a perdu.
+Le programme doit arbitrer une partie se déroulant entre deux joueurs humains, dont leur nom est
+demandé au démarrage, ainsi que le joueur qui commence.
+"""
